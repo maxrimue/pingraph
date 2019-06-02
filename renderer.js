373407c8cf3f85a92d91fc45ef640474c27b9f6d
Max Rittmüller <max.rittmueller@gmail.com>
2019-06-02 17:04:13 +0200

    remove chart.js

--- renderer.js
+++ renderer.js
@@ -1,36 +1,5 @@
 // soos
 const ping = require("./ping");
-const Chart = require("chart.js");
-
-var ctx = document.getElementById("pings").getContext("2d");
-var myChart = new Chart(ctx, {
-  type: "line",
-  data: {
-    labels: ["1", "2", "3", "4"],
-    datasets: [
-      {
-        label: "ping",
-        data: [4, 8, 2, 2]
-      }
-    ]
-  },
-  options: {
-    scales: {
-      xAxes: [
-        {
-          display: false
-        }
-      ],
-      yAxes: [
-        {
-          display: false
-        }
-      ]
-    }
-  },
-  maintainAspectRatio: false,
-  responsive: true
-});
 
 // const pingsElement = document.querySelector("#pings");
 
